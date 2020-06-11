9ed939883ed3019de093dcfdbbdfd31fed543311
To_Ran <41686179+biru100@users.noreply.github.com>
2020-06-12 04:54:52 +0900

    플밍 11강

---
+++ 게임프로그래밍의 이해/11강/202013065_김효장_P80.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Break
+{
+    static void Main()
+    {
+        int a, b = 2;
+
+        for (a = 0; a < 5; a++)
+        {
+            if (b - a <= 0)
+                break;
+            Console.WriteLine(b + " - " + a + " = " + (b - a));
+        }
+    }
+}
