7dc3a0e907c004506ba8992a44d4c07bf215ee2b
To_Ran <41686179+biru100@users.noreply.github.com>
2020-06-25 21:09:12 +0900

    12 13강

---
+++ 게임프로그래밍의 이해/12강/202013065_김효장_별찍기프로그램.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            for (int n = 0; n < value; n++)
+            {
+                for (int i = 0; i < value; i++)
+                {
+                    for (int j = 0; j < value; j++)
+                    {
+                        for (int k = 0; k < value - i; k++)
+                        {
+                            if ((j + n) % 2 == 0)
+                                Console.Write("*");
+                            else
+                                Console.Write(" ");
+                        }
+                        for (int k = 0; k < i * 2 + 1; k++)
+                        {
+                            if ((j + n) % 2 == 0)
+                                Console.Write(" ");
+                            else
+                                Console.Write("*");
+                        }
+                        for (int k = 0; k < value - i; k++)
+                        {
+                            if ((j + n) % 2 == 0)
+                                Console.Write("*");
+                            else
+                                Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine(" ");
+                }
+                for (int i = 0; i < value - 1; i++)
+                {
+                    for (int j = 0; j < value; j++)
+                    {
+                        for (int k = 0; k < i + 2; k++)
+                        {
+                            if ((j + n) % 2 == 0)
+                                Console.Write("*");
+                            else
+                                Console.Write(" ");
+                        }
+                        for (int k = 0; k < (value - i - 1) * 2 - 1; k++)
+                        {
+                            if ((j + n) % 2 == 0)
+                                Console.Write(" ");
+                            else
+                                Console.Write("*");
+                        }
+                        for (int k = 0; k < i + 2; k++)
+                        {
+                            if ((j + n) % 2 == 0)
+                                Console.Write("*");
+                            else
+                                Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
