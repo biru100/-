1b8e73aaab497fdb92d1d59816544b5f027f9a7c
To_Ran <41686179+biru100@users.noreply.github.com>
2021-04-01 14:08:49 +0900

    ㅁㄴㅇㄹ

---
+++ 2021/게임네트워크프로그래밍/2주차/Week2_202013065_Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class StreamTcpSrvr
+{
+    static void Main(string[] args)
+    {
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+        TcpListener server = new TcpListener(ipep);
+        server.Start();
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected with {0} at port {1}",
+            ((IPEndPoint)client.Client.RemoteEndPoint).Address, ((IPEndPoint)client.Client.RemoteEndPoint).Port);
+        NetworkStream ns = client.GetStream();
+        StreamReader sr = new StreamReader(ns);
+        StreamWriter sw = new StreamWriter(ns);
+
+        int length;
+        string data = null;
+        byte[] bytes = new byte[256];
+        data = "안녕하세요";
+        byte[] msg1 = Encoding.Default.GetBytes(data);
+
+        ns.Write(msg1, 0, msg1.Length);
+        try
+        { 
+            while ((length = ns.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = Encoding.Default.GetString(bytes, 0, length);
+                Console.WriteLine(String.Format("받은 메세지 : {0}", data));
+                Console.Write("메세지를 입력하세요 : ");
+                byte[] msg = Encoding.Default.GetBytes(Console.ReadLine());
+                ns.Write(msg, 0, msg.Length);
+            }
+            Console.WriteLine("Disconnecting from server...");
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Unable to connect to server.");
+            Console.WriteLine(e.ToString());
+            return;
+        }
+        Console.WriteLine("Disconnected from {0}", ((IPEndPoint)client.Client.RemoteEndPoint).Address);
+        sw.Close();
+        sr.Close();
+        ns.Close();
+        client.Close();
+    }
+}
